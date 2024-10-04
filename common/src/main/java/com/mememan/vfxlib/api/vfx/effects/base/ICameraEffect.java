4840de0b80f103a46dbc8f775e2186ff6e4c85c4
Meme Man <81907436+RaveTr@users.noreply.github.com>
2024-10-04 19:52:41 +0300

    vfx work begin something something idk

---
+++ common/src/main/java/com/mememan/vfxlib/api/vfx/effects/base/ICameraEffect.java
@@ -0,0 +1,7 @@
+package com.mememan.vfxlib.api.vfx.effects.base;
+
+/**
+ * A sub-interface of {@link IScreenEffect} that provides more camera-oriented methods for extensive camera effect customizability.
+ */
+public interface ICameraEffect {
+}
