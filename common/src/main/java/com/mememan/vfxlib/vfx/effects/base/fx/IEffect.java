2e9a919ac298cdf98f5107aa5353b25cc3d8b90b
Meme Man <81907436+RaveTr@users.noreply.github.com>
2024-10-11 16:02:47 +0300

    forgor some javadocs momentos :skull:

--- common/src/main/java/com/mememan/vfxlib/vfx/effects/base/fx/IEffect.java
+++ common/src/main/java/com/mememan/vfxlib/vfx/effects/base/fx/IEffect.java
@@ -7,6 +7,13 @@ import net.minecraft.network.chat.Component;
 
 /**
  * The base interface for all VFX effects. <br></br>
+ *
+ * All sub-interfaces representing VFX supercategories are inherently derived from this interface, whose sole purpose is to store common methods that should be shared across all existing effects and their types. <br></br>
+ *
+ * For more specific implementations, refer to the direct sub-interfaces of this interface. <br></br>
+ *
+ * Note that this interface should NOT be used for direct effect implementations, but that it should rather be used as the foundation for more spec-tailored effect interfaces, which are then optimally implemented and fleshed-out
+ * for proper use.
  */
 public interface IEffect {
 
--- common/src/main/java/com/mememan/vfxlib/vfx/effects/base/fx/IScreenEffect.java
+++ common/src/main/java/com/mememan/vfxlib/vfx/effects/base/fx/IScreenEffect.java
@@ -1,11 +1,16 @@
 package com.mememan.vfxlib.vfx.effects.base.fx;
 
+import com.mememan.vfxlib.vfx.effects.base.data.EffectPresence;
+
 /**
  * Hook sub-interface of {@link IEffect} whose sub-interfaces allow for the modification of the client's visual display by providing commonly-used methods that allow for the alteration of rendered elements on-screen (Static/animated overlays,
  * camera effects, level rendering, ghost objects, etc.). <br></br>
  *
  * This interface in and of itself provides the foundation for all types of visual effects. It is intended to be extended by sub-interfaces that provide more specialized methods for specific visual effects rather than
- * being directly implemented by VFX classes.
+ * being directly implemented by VFX classes, as per standard VFXLib conventions. <br></br>
+ *
+ * It should be noted that by default, any effect derived from this interface typically falls under effect types whose effect presence is {@link EffectPresence#GRAPHICAL}. Some exceptions may exist for effects that have some
+ * relationship to the server but should still be manipulated to some extent by the client, such as ghost-object effects, object-tied afterimage overlays, etc.
  */
 public interface IScreenEffect extends IEffect {
 
